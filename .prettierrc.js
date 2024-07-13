0eb35739ed86b0574648cf389b79d1f3b3a22b06
Rickory2021 <rickylego15@gmail.com>
2024-07-13 17:19:19 -0400

    - Update Prettier Config
    Used to help elaborate common formatting.
    Feel free to Edit, Remove, or Add if needed

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
   singleQuote: true,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
   trailingComma: 'all',
+
+  bracketSpacing: false,
+  bracketSameLine: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: false,
 };
